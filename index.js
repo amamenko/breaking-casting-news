7387330d5851cdad6bb852957a188e5e525042a7
Avi Mamenko <avimamenko@gmail.com>
2023-06-12 15:41:32 -0400

    Modified posting CRON job to run every 4 hours and only hit non-premium twitter endpoints

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 const { createPost } = require("./functions/createPost");
-const { TwitterApi } = require("twitter-api-v2");
-const { parseISO, subHours, compareAsc } = require("date-fns");
+const { TwitterApi } = require("twitter-api-v2"); 
 const cron = require("node-cron");
 const { deployToRender } = require("./functions/deployToRender");
 require("dotenv").config();
@@ -16,42 +15,9 @@ const client = new TwitterApi({
   accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-// Try to post every 50 minutes every 4-5 hours
-cron.schedule("*/50 0,1,4,5,8,9,12,13,16,17,20,21 * * *", async () => {
-  const tweetsOfBCN = await client.v2.userTimeline("1488719370692050944", {
-    exclude: "replies",
-    "tweet.fields": ["created_at"],
-  });
-  if (tweetsOfBCN) {
-    if (tweetsOfBCN._realData) {
-      if (tweetsOfBCN._realData.data) {
-        const mostRecentTweet = tweetsOfBCN._realData.data[0];
-
-        if (mostRecentTweet) {
-          const mostRecentTimePosted = parseISO(mostRecentTweet.created_at);
-          const twoHoursAgo = subHours(new Date(), 2);
-
-          // Returns -1 if tweet was made more than two hours ago and 1 if most recent tweet was made less than two hours ago
-          const comparison = compareAsc(mostRecentTimePosted, twoHoursAgo);
-
-          if (comparison === -1) {
-            console.log(
-              "No tweet posted yet in the last two hours! Attempting to create post..."
-            );
-            createPost();
-          } else {
-            console.log("Already posted in the last two hours!");
-            return;
-          }
-        } else {
-          console.log("Couldn't find most recent tweet!");
-          return;
-        }
-      } else {
-        createPost();
-      }
-    }
-  }
+// Try to post every 4 hours
+cron.schedule("0 */4 * * *", async () => {
+  createPost();
 });
 
 app.get("/", (req, res) => {
